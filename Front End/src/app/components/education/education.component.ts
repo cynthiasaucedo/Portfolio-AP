91cc9431ccc69eb0d9372e7c9fa7e233744ca897
Cynthia Saucedo <cynthiasaucedo97@gmail.com>
2022-06-26 17:10:22 -0300

    Angular project

---
+++ Front End/src/app/components/education/education.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
+
+@Component({
+  selector: 'education',
+  templateUrl: './education.component.html',
+  styleUrls: ['./education.component.css']
+})
+export class EducationComponent implements OnInit {
+  miPortfolio:any;
+  constructor(private datosPortfolio:PortfolioService) { }
+
+  ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data =>{
+      console.log(data);
+      this.miPortfolio=data;
+    });
+  }
+
+}
